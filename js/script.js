febba9ed403af8494424e28d8ae3d09882866fc8
caiovncs <devcaiovinicius@gmail.com>
2023-11-14 00:06:50 -0300

    feat: adicionada seção de faq

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const accordionList = document.querySelectorAll(['[data-anime="accordion"] dt']);
+
+function toggleAccordion(item) {
+  item.nextElementSibling.classList.toggle('ativo');
+  item.classList.toggle('ativo');
+
+}
+
+function addDtEvent() {
+  accordionList.forEach((item) => {
+    item.addEventListener('click', () => toggleAccordion(item));
+  });
+}
+
+function init() {
+  if (accordionList.length) {
+    toggleAccordion(accordionList[0]);
+    addDtEvent();
+  }
+  return this;
+}
+
+init();
