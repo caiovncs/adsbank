b816bca31957a776285381294c33ece36e79bbfe
caiovncs <devcaiovinicius@gmail.com>
2023-11-14 21:23:56 -0300

    feat: seção download

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const accordionList = document.querySelectorAll(['[data-anime="accordion"] dt'])
 function toggleAccordion(item) {
   item.nextElementSibling.classList.toggle('ativo');
   item.classList.toggle('ativo');
-
 }
 
 function addDtEvent() {
